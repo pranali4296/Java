8c0fbd186bf5e03267e2f997b6ee7410b5c93d8c
pranali mhatre <pranali.mhatre@infostretch.com>
2021-12-23 10:52:42 +0530

    Task Commited

---
+++ String/src/StringBuilderExample.java
@@ -0,0 +1,10 @@
+
+public class StringBuilderExample {
+	public static void main(String args[]){  
+		StringBuilder sb=new StringBuilder("Hello ");  
+		sb.append("Java");//now original string is changed  
+		System.out.println(sb);//prints Hello Java  
+		}  
+}
+		
+
